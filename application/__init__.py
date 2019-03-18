decb0d87ec21644a0679e02a95bba27453af7496
uberballo <kasper.paavolainen@helsinki.fi>
2019-03-18 14:24:16 +0200

    Configured __init__ to fit heroku.

--- application/__init__.py
+++ application/__init__.py
@@ -1,12 +1,18 @@
-#flaski 
+#flask 
 from flask import Flask
 app = Flask(__name__)
 
-#tietokanta
+import os
+
+#database
 from flask_sqlalchemy import SQLAlchemy
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///items.db"
-app.config["SQLALCHEMY_ECHO"] = True
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+
+if os.environ.get("HEROKU"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+else:   
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///items.db"
+    app.config["SQLALCHEMY_ECHO"] = True
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 
 db = SQLAlchemy(app)
 
@@ -39,6 +45,7 @@ login_manager.login_message = "Please log in to use the pawnstore"
 def load_user(user_id):
     return User.query.get(user_id)
 
-db.create_all()
-
-db.create_all()
+try:
+    db.create_all()
+except: 
+    pass
