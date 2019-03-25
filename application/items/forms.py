30f12d57d9fee2d2795d095115d7763f154e2c4b
uberballo <kasper.paavolainen@helsinki.fi>
2019-03-25 22:22:26 +0200

    Made base object for db and fixed typos

--- application/items/forms.py
+++ application/items/forms.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, BooleanField, validators
 
 class ItemForm(FlaskForm):
-    name = StringField("Item name", [validators.length(min=2)])
+    name = StringField("Item name", [validators.length(min=2,max=30)])
     available = BooleanField("Available")
  
     class Meta:
--- application/users/forms.py
+++ application/users/forms.py
@@ -2,8 +2,11 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, validators
 
 class UserForm(FlaskForm):
-    username = StringField("Username",[validators.length(min=2)])
-    password = PasswordField("Password") 
+    username = StringField("Username"
+    ,[validators.length(min=2,max=30)])
+    password = PasswordField("Password",[validators.InputRequired()
+    ,validators.EqualTo('confirm',message= 'Passwords must match')])
+    confirm = PasswordField('Repeact password')
 
     class Meta:
         csrf = False
